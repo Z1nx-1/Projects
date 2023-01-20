657254fe71713942be18a69e9b1c9bf974f7bfe6
Jack <86924197+Z1nx-1@users.noreply.github.com>
2023-01-20 02:18:12 +0000

    Add files via upload

---
+++ Caculator.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter second Number: "))
+
+result = num1 + num2
+
+print(result)
+
+Quit = input("Quit? Y/N:")
+if Quit == "Y":
+    quit
